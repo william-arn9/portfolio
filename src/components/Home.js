bc5416deb035f2050d70151027e61b59d4d7dc9a
William Arnold <williamarn99@gmail.com>
2024-08-19 17:07:53 -0700

    Some visual updates and improved json viewer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TextTransition, { presets } from 'react-text-transition';
 import './Home.scss';
 
 const Home = () => {
+  const [index, setIndex] = useState(0);
+
+  const TEXTS = [ 'fullstack', 'frontend ' ];
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      4500,
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
   return (
     <div className="home">
-      <h1>Are you looking for a quality <span className="emphasis">fullstack developer?</span></h1>
+      <h1>
+        Are you looking for a quality <span className="emphasis">
+        <span className="transition-text">
+          {TEXTS[index % TEXTS.length].split('').map((l, i) => (
+            <TextTransition key={i} delay={i * 50} springConfig={presets.gentle} translateValue='50%' inline>{l}</TextTransition>
+          ))}</span> developer?
+        </span>
+      </h1>
       <h2>Well, look no further...</h2>
     </div>
   );
--- src/components/JsonViewer.js
+++ src/components/JsonViewer.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
+import JSONInput from 'react-json-editor-ajrm';
+import locale from 'react-json-editor-ajrm/locale/en';
 import './JsonViewer.scss';
 
 function JsonViewer() {
   const [inputJson, setInputJson] = useState('');
-  const [formattedJson, setFormattedJson] = useState('');
+  const [jsonObject, setJsonObject] = useState(null);
+  const [errorObject, setErrorObject] = useState(null);
 
   const handleFormat = () => {
     try {
       console.log(inputJson);
       const parsedJson = JSON.parse(inputJson);
-      const prettyJson = JSON.stringify(parsedJson, null, 2);
-      setFormattedJson(prettyJson);
+      console.log(parsedJson);
+      setJsonObject(parsedJson);
+      setErrorObject(null);
     } catch (error) {
-      setFormattedJson('Invalid JSON');
+      setJsonObject(null);
+      setErrorObject({reason: error});
     }
   };
 
@@ -28,15 +33,36 @@ function JsonViewer() {
           ></textarea>
         </div>
         <div className="action pane">
-          <button type="button" onClick={() => {setInputJson('');setFormattedJson('')}}>Clear</button>
+          <button type="button" onClick={() => {setInputJson('');setErrorObject(null);setJsonObject(null)}}>Clear</button>
           <button type="button" onClick={handleFormat}>View Formatted</button>
         </div>
         <div className="read pane">
-          <textarea
-            placeholder="<Formatted JSON will appear here>"
-            disabled
-            value={formattedJson}
-          ></textarea>
+          <JSONInput
+            id='results'
+            placeholder={jsonObject ?? [ ]}
+            locale={ locale }
+            height='80vh'
+            reset
+            error={errorObject}
+            style={{
+              body: {height: '100%', padding: '12px 4px', "text-align": "left", "border-radius": "8px"},
+              container: {width: '100%', height: '100%', "border-radius": "8px"},
+              outerBox: {width: '96%', height: '100%'},
+              warningBox: {height: '0px', 'text-align': 'left', margin: '0 0 0 30px'}
+            }}
+            colors={{
+              default            : '#D4D4D4',
+              background         : '#ffffff20',
+              background_warning : '#1E1E1E',
+              string             : '#CE8453',
+              number             : '#B5CE9F',
+              colon              : '#7FFFD4',
+              keys               : '#7FFFD4',
+              keys_whiteSpace    : '#AF74A5',
+              primitive          : '#6392C6'
+            }}
+            viewOnly
+          />
         </div>
       </section>
     </div>

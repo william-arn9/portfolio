bc0ad51eeec3b04a033c086f35eb6f1200d80866
William Arnold <williamarn99@gmail.com>
2024-08-26 18:08:10 -0700

    Updating LH integration

--- server/index.mjs
+++ server/index.mjs
@@ -61,7 +61,7 @@ app.get('/api/lighthouse', async (req, res) => {
     });
 
     const page = await browser.newPage();
-    await page.goto(`http://localhost:${PORT}`);  // Replace with your local or deployed URL as appropriate
+    await page.goto(`http://localhost:${PORT}`, { waitUntil: 'networkidle0' });  // Replace with your local or deployed URL as appropriate
 
     const options = {
       logLevel: 'info',

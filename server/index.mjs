7c87b0ba4ac331fd224529a0d938c0c41f0ec639
William Arnold <williamarn99@gmail.com>
2024-09-04 17:43:59 -0700

    Improved email integration spec

--- server/index.mjs
+++ server/index.mjs
@@ -124,6 +124,7 @@ app.post('/send-email', contactLimiter, async (req, res) => {
     service_id: process.env.EMAILJS_SERVICE_ID,
     template_id: process.env.EMAILJS_TEMPLATE_ID,
     user_id: process.env.EMAILJS_PUBLIC_KEY,
+    accessToken: process.env.EMAILJS_PRIVATE_KEY,
     template_params: {
       from_name: sanitizedName,
       from_email: sanitizedEmail,
